e7f526a41019828a9da1eaa9adbe760184f50ac5
David Alejandro Aguirre Morales <david.aguirre1598@outlook.com>
2020-07-22 12:32:27 -0500

    Primer lanzamiento, funcionamiento básico y con características avanzadas aún no implementadas

---
+++ i2c.c
@@ -0,0 +1,149 @@
+/*
+ * File:   i2c.c
+ * Autor:  David A. Aguirre Morales - david.aguirre1598@outlook.com
+ *
+ * Fecha de creación:   23 de junio de 2020, 08:33 PM
+ * Última modificación: 22 de julio de 2020
+ *                      documentación, descripción de lo que se ha realizado y
+ *                      futuras actualizaciones
+ * 
+ * Descripción :
+ * Libreria para la implementación del periferico USI en modo I²C.
+ * Declaración de funciones
+ * 
+ * ESTADO:
+ *  SOLO LEE O ESCRIBE UN REGISTRO A LA VEZ (IMPORTANTE)
+ * 
+ * FUTURAS ACTUALIZACIONES:
+ *  IMPLEMENTACIÓN ESCRITURA Y LECTURA DE VARIOS REGISTROS CONSECUTIVOS SEGÚN EL 
+ *  ESTANDAR.
+ */
+
+#include "i2c.h"
+#include <avr/interrupt.h>
+
+#define SDAP  PIN0
+#define SCLP  PIN2
+#define I2CD  DDRB
+#define I2CP  PORTB
+
+static uint8_t status = 0;                  /*Status (Estado Actual)*/
+static uint8_t data   = 0;                  /*Data*/
+static uint8_t rdir   = 0;                  /*Register direction*/
+static uint8_t ack    = 0;                  /*ACK (Indicador de modo ACK)*/
+
+/*Interrupciones*/
+ISR(USI_START_vect){
+    I2CD |= ( 1<<SCLP );                    /*Mantener SCL*/
+	
+    if (status == 2) {                      /*Repeated START*/
+        status = 0;                         /*Lea de nuevo la dirección*/
+    }
+    else {	
+        USICR |= (1<<USIOIE);               /*Interrupcion por desborde*/
+        USIDR = 0;
+    }
+	
+    USISR =  ~((1<<USICNT3) |               /*Reinicio de banderas*/
+               (1<<USICNT2) |               /*y el contador*/
+               (1<<USICNT1) |
+               (1<<USICNT0));
+	
+    I2CD &= ~(( 1<<SCLP ));                 /*Liberar SCL*/
+}
+
+ISR(USI_OVF_vect){
+    if(ack){
+
+        if ( status == 4) {                 /*Modo envío de datos (PRE)*/
+            USIDR = i2c_slave.registers[rdir];
+            I2CP |=  ( 1<<SDAP );           /*SDA como salida, para envío*/
+        }
+        else {
+            I2CD &= ~(( 1<<SDAP ));         /*Liberar SDA, para el resto de */
+        }                                   /*modos*/
+
+        if(status == 3) {
+            i2c_slave.registers[rdir] = data;
+            USICR &= ~(1<<USIOIE);
+            status = 0;
+            USISR |= (1<<USIPF);
+        }
+        ack = 0;
+        USISR &= ~((1<<USICNT3) |           /*Reinicio de banderas*/
+                   (1<<USICNT2) |           /*y el contador*/
+                   (1<<USICNT1) |
+                   (1<<USICNT0));
+    }
+    else {
+        I2CD |=  ( 1<<SCLP );               /*Mantener SCL en bajo*/
+
+        if(status == 0){                    /*Lectura de direccion (e) y modo*/
+            uint8_t wrrd = USIDR&0x1;       /*Write / Read*/
+            uint8_t tdir = USIDR>>1;        /*Temporal direction (readed)*/
+            i2c_slave.cmode = wrrd;
+            if((i2c_slave.cmode == 1) &&
+               (i2c_slave.direction == tdir)) {
+                    I2CD |= ( 1<<SDAP );    /*ACK*/
+                    ack = 1;
+                    status = 4;
+            }
+            else if(tdir == i2c_slave.direction){
+                    I2CD |=  ( 1<<SDAP );   /*ACK*/
+                    ack = 1;
+                    status++;
+            }
+        }
+        else if(status == 1){               /*Lectura de dirección de registro*/
+            rdir = USIDR;                   /*register direction*/
+            I2CD	|=  ( 1<<SDAP );    /*ACK*/
+            ack = 1;
+            status++;
+        }
+        else if(status == 2){               /*Lectura de los datos entrantes*/
+            data  = USIDR;                   
+            I2CD |= ( 1<<SDAP );            /*ACK*/
+            ack = 1;
+            status++;
+        }
+        else if(status == 4){               /*Modo de envió de datos (POST)*/
+            I2CP &= ~( 1<<SDAP );           /*Apague la salida*/
+            I2CD &= ~( 1<<SDAP );			
+            while(!(PINB&(1<<SDAP)));       /*Espere NACK*/
+            status = 0;                     /*Reincie el sistema*/
+        }
+        USISR = ~USISR &                    /*Reinicio contador*/
+                ~(( 1<<USICNT3 )|
+                  ( 1<<USICNT2 )|
+                  ( 1<<USICNT1 )|
+                  ( 1<<USICNT0 ));
+        if(ack) {
+            USISR |= ( 1<<USICNT3 )|        /*Inicializacion contador en 14*/
+                     ( 1<<USICNT2 )|
+                     ( 1<<USICNT1 );
+        }
+        USIDR    = 0;                       /*Limpieza buffer entrada*/
+        USISR	|= ( 1<<USIOIF ) |          /*Limpieza banderas*/
+                   ( 1<<USISIF ) ;
+
+        I2CD &= ~( 1<<SCLP );               /*Liberar SCL*/
+    }
+
+}
+
+void usi_i2c_slave(uint8_t dir){
+    I2CP &= ~(( 1<<SDAP ) | ( 1<<SCLP ));   /*Configuracion pines SDA y SCL*/
+    I2CD &= ~(( 1<<SDAP ) | ( 1<<SCLP ));
+
+    USICR =  ( 1<<USISIE )|                 /*Interrupcion START*/
+             ( 1<<USIWM1 )|                 /*Modo i2c*/
+             ( 1<<USICS1 );                 /*con fuente de reloj externo*/
+
+    USISR =  ( 1<<USISIF )|                 /*Limpieza de banderas*/
+             ( 1<<USIOIF )| 
+             ( 1<<USIPF  )| 
+             ( 1<<USIDC  );
+
+    i2c_slave.direction = dir;              /*Asignacion de dirección*/
+    sei();                                  /*Interrupciones globales*/
+}
---
+++ i2c.h
@@ -0,0 +1,55 @@
+/*
+ * File:   i2c.h
+ * Autor:  David A. Aguirre Morales david.aguirre1598@outlook.com
+ *
+ * Fecha de creación:   23 de junio de 2020, 08:35 PM
+ * Última modificación: 22 de julio de 2020
+ *						documentación
+ * 
+ * Descripción:
+ * Libreria para la implementación del periferico USI en modo I²C.
+ * Descripción de funciones.
+ * 
+ *  * ESTADO:
+ *  SOLO LEE O ESCRIBE UN REGISTRO A LA VEZ (IMPORTANTE)
+ * 
+ * FUTURAS ACTUALIZACIONES:
+ *  IMPLEMENTACIÓN ESCRITURA Y LECTURA DE VARIOS REGISTROS CONSECUTIVOS SEGÚN EL 
+ *  ESTANDAR.
+ */
+
+/* ESTADO HEADER
+ *	DESCRIPCIÓN BÁSICA DE LA FUNCIÓN
+ * 
+ * FUTURAS ACTUALIZACIONES:
+ *	DOCUMENTACIÓN
+ */
+
+#ifndef _I2C_H_
+#define	_I2C_H_
+#include <stdint.h>
+
+/* i2c_slave ESTRUCTURA
+ *	cmode: current mode: Modo actual del maestro (0) Escritura (1) Lectura.
+ *	direction: Dirección del esclavo.
+ *	registers: Registros del periferico para ser leídos o escritos, hasta 128.
+ */
+struct i2c_slave{
+	uint8_t cmode		:1;
+	uint8_t direction	:7;
+	uint8_t registers[128];
+} i2c_slave;
+
+/*----------------------------------------------------------------------------*/
+
+/* usi_i2c_slave()
+ * Descripción:
+ *	Inicialización del periferico USI para trabajar el protocolo I²C en modo
+ *  esclavo.		
+ * Argumentos:
+ *	-> dir:	Direccion deseada del modo esclavo
+ * Retorno:
+ *	<- ninguno */
+void usi_i2c_slave(uint8_t dir);
+
+#endif	/* _I2C_H */
