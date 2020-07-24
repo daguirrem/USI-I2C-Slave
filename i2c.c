5e3fc0f2e6359bed05df9b9563b2c4fe95ba1907
David Alejandro Aguirre Morales <david.aguirre1598@outlook.com>
2020-07-24 14:05:42 -0500

    nuevo formato, implementación de lectura y escritura consecutivos registros.

--- i2c.c
+++ i2c.c
@@ -4,162 +4,204 @@
  *
  * Fecha de creación:   23 de junio de 2020, 08:33 PM
  * Última modificación: 22 de julio de 2020
- *                      documentación, descripción de lo que se ha realizado y
- *                      futuras actualizaciones
- * 
+ *                      nuevo formato,
+ *			implementación de lectura y escritura consecutivos registros.
+ *
  * Descripción :
- * Libreria para la implementación del periferico USI en modo I²C.
- * Declaración de funciones
- * 
- * ESTADO:
- *  SOLO LEE O ESCRIBE UN REGISTRO A LA VEZ (IMPORTANTE)
- * 
- * FUTURAS ACTUALIZACIONES:
- *  IMPLEMENTACIÓN ESCRITURA Y LECTURA DE VARIOS REGISTROS CONSECUTIVOS SEGÚN EL 
- *  ESTANDAR.
+ *  Libreria para la implementación del periferico USI en modo I²C.
+ *  Declaración de funciones e interrupciones.
+ *
+ * Estado:
+ *  Funcionalidad probada y funcionando.
+ *
+ * Futuras actualizaciones:
+ *  Implementación de funciones guardar distintos tipos de datos en los registros.
  */
 
 #include "i2c.h"
 #include <avr/interrupt.h>
 
-#define SDAP  PIN0
-#define SCLP  PIN2
-#define I2CD  DDRB
-#define I2CP  PORTB
+#define SDAP  PIN0		/*#PIN correspondiente al SDA en el puerto*/
+#define SCLP  PIN2		/*#PIN correspondiente al SCL en el puerto*/
 
-static uint8_t status = 0;                  /*Status (Estado Actual)*/
-static uint8_t data   = 0;                  /*Data*/
-static uint8_t rdir   = 0;                  /*Register direction*/
-static uint8_t ack    = 0;                  /*ACK (Indicador de modo ACK)*/
+#define I2CPN PINB		/*Registro PINx donde está el periférico I²C*/
+#define I2CD  DDRB		/*Registro DDRx donde está el periférico I²C*/
+#define I2CP  PORTB		/*Registro PORTx donde está el periférico I²C*/
+
+static uint8_t status = 0;	/*Status (Estado Actual)		*/
+static uint8_t rdir   = 0;	/*Register direction (Dirección actual)	*/
+static uint8_t ack    = 0;	/*ACK (Indicador de modo ACK)		*/
 
 /*Interrupciones*/
+
+/*Interrupción por detección de START*/
 ISR(USI_START_vect){
-    I2CD |= ( 1<<SCLP );                    /*Mantener SCL*/
-	
-    if (status == 2) {                      /*Repeated START*/
-        status = 0;                         /*Lea de nuevo la dirección*/
+    /*Mantener SCL*/
+    I2CD |= ( 1<<SCLP );
+    /*¿Repeated START?*/
+    if (status == 2) {
+	/*Si, Vuelva al inicio para que lea de nuevo la dirección*/
+        status = 0;
     }
-    else {	
-        USICR |= (1<<USIOIE);               /*Interrupcion por desborde*/
+    else {
+	/*No, prepare la interrupción por desborde*/
+        USICR |= (1<<USIOIE);
         USIDR = 0;
     }
-	
-    USISR =  ~((1<<USICNT3) |               /*Reinicio de banderas*/
-               (1<<USICNT2) |               /*y el contador*/
-               (1<<USICNT1) |
-               (1<<USICNT0));
-	
-    I2CD &= ~(( 1<<SCLP ));                 /*Liberar SCL*/
+    /*Reinicio de todas la banderas y del contador*/
+    USISR =  ~( (1<<USICNT3)|(1<<USICNT2)|(1<<USICNT1)|(1<<USICNT0) );
+    /*Liberar SCL*/
+    I2CD &= ~(( 1<<SCLP ));
 }
 
 ISR(USI_OVF_vect){
-    
+
+    /*¿Modo ACK? (¿Estoy en el bit correspondiente al ACK?)*/
     if(ack){
-	
-	if(status == 5){		    /*¿NACK o ACK? (por parte del*/
-	    if ( bit_is_clear(PINB,SDAP)) { /*maestro)*/
-		status = 4;		    /*En caso de ACK, prepare el*/
-		I2CD |=  ( 1<<SDAP );	    /*siguiente envío del registro*/
+
+	 /*¿NACK o ACK? (por parte del maestro)*/
+	if(status == 5){
+	    if ( bit_is_clear(I2CPN,SDAP)) {
+		/*En caso de ACK, prepare el siguiente envío del registro*/
+		status = 4;
+		I2CD |=  ( 1<<SDAP );
+		rdir++;
 	    }
-	    else {			    /*En caso de NACK, termine la */
-		status = 0;		    /*trasmisión*/
+	    else {
+		/*En caso de NACK, termine la trasmisión*/
+		status = 0;
+		rdir = 0;
 		I2CP &= ~(( 1<<SDAP ));
 		I2CD &= ~(( 1<<SDAP ));
 		USICR &= ~(1<<USIOIE);
 	    }
 	}
-	if ( status == 4) {                 /*Modo envío de datos (PRE)*/
-	    I2CD |=  ( 1<<SCLP );           /*Mantener SCL en bajo*/
+	/*Modo recepción de datos (POST ACK)*/
+	if(status == 3) {
+	    /*Mantener SCL en bajo*/
+            I2CD |=  ( 1<<SCLP );
+	    /*¿Stop?*/
+	    if(bit_is_set(USISR,USIPF)){
+	    /*Si, Detenga la trasmición*/
+		rdir = 0;
+		status = 0;
+		USICR &= ~(1<<USIOIE);
+	    }
+	    else {
+	    /*No, Prepare el siguiente registro*/
+		status=2;
+		rdir++;
+	    }
+	    /*Liberar SDA, para el resto de modos*/
+            I2CD &= ~(( 1<<SDAP ));
+        }
+	/*Modo envío de datos (PRE)*/
+	else if ( status == 4) {
+	     /*Mantener SCL en bajo*/
+	    I2CD |=  ( 1<<SCLP );
+	    /*Cargue el registro de salido con los datos*/
             USIDR = i2c_slave.registers[rdir];
-            I2CP |=  ( 1<<SDAP );           /*SDA como salida, para envío*/
+	    /*SDA como salida, para envío*/
+            I2CP |=  ( 1<<SDAP );
         }
         else {
-	    I2CD |=  ( 1<<SCLP );           /*Mantener SCL en bajo*/
-            I2CD &= ~(( 1<<SDAP ));         /*Liberar SDA, para el resto de */
-        }                                   /*modos*/
-
-        if(status == 3) {		    /*Escritura por parte del maestro*/
-            i2c_slave.registers[rdir] = data;
-            USICR &= ~(1<<USIOIE);
-            status = 0;
+	    /*Mantener SCL en bajo*/
+	    I2CD |=  ( 1<<SCLP );
+	    /*Liberar SDA, para el resto de modos*/
+            I2CD &= ~(( 1<<SDAP ));
         }
-	
+
+	/*Alterne el modo ACK*/
 	ack = 0;
-        USISR &= ~((1<<USICNT3) |           /*Reinicio de banderas*/
-                   (1<<USICNT2) |           /*y el contador*/
-                   (1<<USICNT1) |
-                   (1<<USICNT0));
-	
-        
+        /*Reinicio de todas la banderas y del contador*/
+	USISR =  ~( ( 1<<USICNT3 )|( 1<<USICNT2 )|( 1<<USICNT1 )|( 1<<USICNT0 ) );
     }
     else {
-	I2CD |=  ( 1<<SCLP );               /*Mantener SCL en bajo*/
-	USISR = ~USISR &                    /*Reinicio contador*/
-                ~(( 1<<USICNT3 )|
-                  ( 1<<USICNT2 )|
-                  ( 1<<USICNT1 )|
-                  ( 1<<USICNT0 ));
-	
-        if(status == 0){                    /*Lectura de direccion (e) y modo*/
-            uint8_t wrrd = USIDR&0x1;       /*Write / Read*/
-            uint8_t tdir = USIDR>>1;        /*Temporal direction (readed)*/
-            i2c_slave.cmode = wrrd;
-            if((i2c_slave.cmode == 1) &&
-               (i2c_slave.direction == tdir)) {
-                    I2CD |= ( 1<<SDAP );    /*ACK*/
-                    ack = 1;
-                    status = 4;
-            }
-            else if(tdir == i2c_slave.direction){
-                    I2CD |=  ( 1<<SDAP );   /*ACK*/
-                    ack = 1;
-                    status++;
-            }
-        }
-        else if(status == 1){               /*Lectura de dirección de registro*/
-            rdir = USIDR;                   /*register direction*/
-            I2CD |=  ( 1<<SDAP );	    /*ACK*/
-            ack = 1;
-            status++;
-        }
-        else if(status == 2){               /*Lectura de los datos entrantes*/
-            data  = USIDR;                   
-            I2CD |= ( 1<<SDAP );            /*ACK*/
-            ack = 1;
-            status++;
-        }
-        else if(status == 4){               /*Modo de envió de datos (POST)*/
-            status++;			    /*Prepare lectura de ACK o NACK*/
-	    USISR |= ( 1<<USICNT0 );	    /*para siguiente flanco*/
-	    I2CD  &= ~( 1<<SDAP);	    /*SDA como entrada*/
-	    ack = 1;
-        }
-        if(ack) {
-            USISR |= ( 1<<USICNT3 )|        /*Inicializacion contador en 14*/
-                     ( 1<<USICNT2 )|
-                     ( 1<<USICNT1 );
-        }
-        USIDR    = 0;                       /*Limpieza buffer entrada*/
-        USISR	|= ( 1<<USIOIF ) |          /*Limpieza banderas*/
-                   ( 1<<USISIF ) ;
+	/*Mantener SCL en bajo*/
+	I2CD |=  ( 1<<SCLP );
+	/*Reiniciar únicamente el contador*/
+	USISR = ~USISR & ~(( 1<<USICNT3 )|( 1<<USICNT2 )|( 1<<USICNT1 )|( 1<<USICNT0 ));
 
+	/*Lectura de direccion (Esclavo) y modo (Escribir o Leer)*/
+	if(status == 0){
+            uint8_t wrrd = USIDR&0x1;	/*Escribir o leer*/
+            uint8_t dire = USIDR>>1;	/*Dirección leída del maestro*/
+
+	    /*¿El maestro envió mi dirección?*/
+	    if(dire == i2c_slave.direction) {
+		/*Compruebe si el maestro quiere escribir o leer*/
+		if(wrrd == 1) {
+		    /*Si quiere leer, active el modo envío de datos*/
+		    status = 4;
+		}
+		else {
+		    /*Si no, lea el registro objetivo*/
+		    status++;
+		}
+		/*Prepare el modo ACK*/
+		I2CD |=  ( 1<<SDAP );
+		ack = 1;
+	    }
+        }
+	/*Lectura de dirección de registro objetivo*/
+        else if(status == 1) {
+            /*Guarde la dirección del registro objetivo*/
+	    rdir = USIDR;
+	    /*Prepare el modo ACK*/
+            I2CD |=  ( 1<<SDAP );
+            ack = 1;
+	    /*Prepare modo recepción de datos (PRE ACK)*/
+            status++;
+        }
+	/*Modo recepción de datos (PRE ACK)*/
+        else if (status == 2) {
+            /*Guarde los datos enviados por el maestro en la dirección dada*/
+	    i2c_slave.registers[rdir] = USIDR;
+	    /*Prepare el modo ACK*/
+            I2CD |= ( 1<<SDAP );
+	    ack = 1;
+            /*Prepare modo recepción de datos (POST ACK)*/
+	    status++;
+        }
+	/*Modo de envió de datos (PRE ACK)*/
+        else if(status == 4) {
+            /*Prepare la interrupción al siguiente flanco de subida en SCL*/
+	    /*(Flanco correspondiente al ACK)*/
+	    USISR |= ( 1<<USICNT0 );	    /*14+1 = 15*/
+	    /*Modo ACK*/
+	    I2CD  &= ~( 1<<SDAP);
+	    ack = 1;
+	    /*Prepare lectura de ACK o NACK*/
+	    status++;
+        }
+
+	/*Si el modo ACK fue configurado inicialice el contador en 14*/
+	/*para provocar una interrupción en el siguiente clock en SCL*/
+        if(ack) {
+            USISR |= ( 1<<USICNT3 )|( 1<<USICNT2 )|( 1<<USICNT1 );
+        }
+	/*Limpieza buffer entrada*/
+        USIDR    = 0;
+	/*Limpieza banderas de interrupción*/
+        USISR	|= ( 1<<USIOIF )|( 1<<USISIF ) ;
     }
-    I2CD &= ~( 1<<SCLP );		    /*Liberar SCL*/
+    /*Liberar SCL*/
+    I2CD &= ~( 1<<SCLP );
 }
 
 void usi_i2c_slave(uint8_t dir){
-    I2CP &= ~(( 1<<SDAP ) | ( 1<<SCLP ));   /*Configuracion pines SDA y SCL*/
+    I2CP &= ~(( 1<<SDAP ) | ( 1<<SCLP ));   /*Configuración pines SDA y SCL*/
     I2CD &= ~(( 1<<SDAP ) | ( 1<<SCLP ));
 
-    USICR =  ( 1<<USISIE )|                 /*Interrupcion START*/
+    USICR =  ( 1<<USISIE )|                 /*Interrupción START*/
              ( 1<<USIWM1 )|                 /*Modo i2c*/
              ( 1<<USICS1 );                 /*con fuente de reloj externo*/
 
     USISR =  ( 1<<USISIF )|                 /*Limpieza de banderas*/
-             ( 1<<USIOIF )| 
-             ( 1<<USIPF  )| 
+             ( 1<<USIOIF )|
+             ( 1<<USIPF  )|
              ( 1<<USIDC  );
 
-    i2c_slave.direction = dir;              /*Asignacion de dirección*/
+    i2c_slave.direction = dir;              /*Asignación de dirección*/
     sei();                                  /*Interrupciones globales*/
 }
