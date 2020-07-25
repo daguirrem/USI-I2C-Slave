1108b3eb732a1989c822a0e0e351cc222880db17
David Alejandro Aguirre Morales <david.aguirre1598@outlook.com>
2020-07-25 13:57:19 -0500

    Nuevos prototipos de funciones de lectura,actualización a completo

--- include/i2c_slave.h
+++ include/i2c_slave.h
@@ -3,8 +3,9 @@
  * Autor:  David A. Aguirre Morales david.aguirre1598@outlook.com
  *
  * Fecha de creación:   23 de junio de 2020, 08:35 PM
- * Última modificación: 24 de julio de 2020
- *						Nuevos prototipos de funciones
+ * Última modificación: 25 de julio de 2020
+ *                      Nuevos prototipos de funciones de lectura
+ *						Actualización a completo
  * 
  * Descripción:
  *  Libreria para la implementación del periferico USI en modo I²C.
@@ -21,7 +22,7 @@
  *  Completo.
  * 
  * FUTURAS ACTUALIZACIONES:
- *  DOCUMENTACIÓN
+ *  Ninguna
  */
 
 #ifndef _I2C_SLAVE_H_
@@ -37,7 +38,7 @@ struct i2c_slave{
 	uint8_t registers[128];
 } i2c_slave;
 
-/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
 
 /* usi_i2c_slave()
  * Descripción:
@@ -51,15 +52,15 @@ void usi_i2c_slave(uint8_t dir);
 
 /* usi_i2c_save_registers_xy()
  * Descripción:
- *  Guarda un byte (data) en registros del I²C, dependiendo del tipo de dato (xy) dode:
- *	    x: - u, unsigned (Números naturales y cero)
- *         - s, signed   (Números enteros)
+ *  Guarda un dato (data) en registros del I²C, dependiendo del tipo de dato (xy) dode:
+ *	 x: - u, unsigned (Números naturales y cero)
+ *      - s, signed   (Números enteros)
  * 
- *	    y: - 8,  1 byte	 (char)
- *         - 16, 2 bytes (half-word)
- *         - 32, 4 bytes (word)
+ *	 y: - 8,  1 byte  (byte)
+ *      - 16, 2 bytes (word)
+ *      - 32, 4 bytes (dword)
  * Argumentos:
- *  -> data:	 Datos que se desean almacentar
+ *  -> data: Datos que se desean almacentar
  *  -> dir:  Dirección de los registros I²C donde se desea almacenar.
  * Retorno:
  *  <- ninguno */
@@ -69,5 +70,26 @@ void usi_i2c_save_registers_u32(uint32_t data, uint8_t dir);
 void usi_i2c_save_registers_s8 (int8_t   data, uint8_t dir);
 void usi_i2c_save_registers_s16(int16_t  data, uint8_t dir);
 void usi_i2c_save_registers_s32(int32_t  data, uint8_t dir);
+
+/* usi_i2c_read_registers_xy()
+ * Descripción:
+ *  Lee un byte (data) en registros del I²C escritos por el maestro, dependiendo del tipo 
+ *  de dato (xy) dode:
+ *	 x: - u, unsigned (Números naturales y cero)
+ *      - s, signed   (Números enteros)
+ * 
+ *	 y: - 8,  1 byte  (byte)
+ *      - 16, 2 bytes (word)
+ *      - 32, 4 bytes (dword)
+ * Argumentos:
+ *  -> dir:  Dirección de los registros I²C donde se desea leer el dato.
+ * Retorno:
+ *  <- Dato almacenado en los registros */
+uint8_t usi_i2c_read_registers_u8 (uint8_t dir);
+uint16_t usi_i2c_read_registers_u16(uint8_t dir);
+uint32_t usi_i2c_read_registers_u32(uint8_t dir);
+int8_t usi_i2c_read_registers_s8 (uint8_t dir);
+int16_t usi_i2c_read_registers_s16(uint8_t dir);
+int32_t usi_i2c_read_registers_s32(uint8_t dir);
 
 #endif	/* _I2C_SLAVE_H */
