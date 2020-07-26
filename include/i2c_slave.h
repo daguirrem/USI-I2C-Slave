6955d8d2ce9898bc7c46fb8009e3237284379e23
David Alejandro Aguirre Morales <david.aguirre1598@outlook.com>
2020-07-26 15:15:06 -0500

    documentación

--- include/i2c_slave.h
+++ include/i2c_slave.h
@@ -3,9 +3,8 @@
  * Autor:  David A. Aguirre Morales david.aguirre1598@outlook.com
  *
  * Fecha de creación:   23 de junio de 2020, 08:35 PM
- * Última modificación: 25 de julio de 2020
- *                      Nuevos prototipos de funciones de lectura
- *						Actualización a completo
+ * Última modificación: 26 de julio de 2020
+ *                      Documentación
  * 
  * Descripción:
  *  Libreria para la implementación del periferico USI en modo I²C.
@@ -25,6 +24,130 @@
  *  Ninguna
  */
 
+/* Ejemplo de implementación
+ * ...
+ * #include <usi_i2c_slave.h>
+ * ...
+ * int main(void){
+ *  ...
+ *  usi_i2c_slave(direction_of_slave);
+ *  ...
+ *  while(1){
+ *	...
+ *  }
+ * 
+ * }
+ * 
+ * + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
+ * 
+ * Si desea modificar registros del periferico:
+ * 
+ * ...
+ * #include <usi_i2c_slave.h>
+ * ...
+ * int main(void){
+ *  ...
+ *  usi_i2c_slave(direction_of_slave);
+ *  ...
+ *  //Usa un byte de los registros disponibles (IMPORTANTE **)
+ *  usi_i2c_save_registers_s8(signed char var,direction_to_save_data);
+ * 
+ *  //Usa dos bytes de los registros disponibles (IMPORTANTE **)
+ *  usi_i2c_save_registers_u16(unsigned int var,direction_to_save_data);
+ * 
+ *  //Usa cuatro bytes de los registros disponibles (IMPORTANTE **)
+ *  usi_i2c_save_registers_u32(unsigned long var,direction_to_save_data);
+ *  
+ *  etc.
+ *  ...
+ *  while(1) {
+ *	...
+ *  }
+ * 
+ * }
+ * 
+ * **Los registros internos del esclavo están organizados de byte en byte por dirección:
+ * 
+ *    i2c_slave_registers: [0x00] (1 byte)
+ *		           [0x01] (1 byte)
+ *			   [... ] ...
+ *			   [0x7F] (1 byte) (0x7F == 127 CONFIGURABLE)
+ * 
+ *    Si se esribe una variable tipo DWORD (32bits) esta ocupará 4 bytes disponibles:
+ *   
+ *     usi_i2c_save_registers_u32(0xFFC90132,0x00); :
+ *     i2c_slave_registers: [0x00] 0xFF (1 byte)
+ *			    [0x01] 0xC9 (1 byte)
+ *			    [0x02] 0x01 (1 byte)
+ *			    [0x73] 0x32 (1 byte)
+ * 
+ * + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
+ * 
+ * Si desea leer constantemente registros (escritos por el maestro)
+ * 
+ * ...
+ * #include <usi_i2c_slave.h>
+ * ...
+ * int main(void) {
+ *  ...
+ *  usi_i2c_slave(direction_of_slave);
+ *  ...
+ *  long int signed_dword_var = 0;
+ *  unsigned char unsigned_byte_var = 0;
+ *  unsigned int unsigned_word_var = 0;
+ * 
+ *  while(1){
+ *	signed_dword_var = usi_i2c_read_registers_s32(direction_of_data_in_registers);
+ *	unsigned_byte_var = usi_i2c_read_registers_u8(direction_of_data_in_registers);
+ *	unsigned_word_var = usi_i2c_read_registers_u16(direction_of_data_in_registers);
+ *  }
+ * 
+ * }
+ * 
+ * + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
+ * 
+ * -¿Cómo se envían los bytes leídos en mi I2C?
+ *  
+ *  EJEMPLO, EL MAESTRO DESEA LEER UN DWORD EN LOS REGISTROS 0x04 con el valor 0xFFC90132
+ *  DE MI ESCLAVO CON DIRECCIÓN 0x1F
+ * 
+ *  el paquete I2C se eviará de la siguiente manera:
+ * 
+ *     0011111| 0| 0| 00000100| 0|    0011111| 1| 0| 11111111| 0| 11001001| 0| 00000001| 0| 00110010| 1|
+ * | S|   DIRE| W| A|     DIRR| A| RS    DIRE| R| A|	 0xFF|A*|     0xC9|A*|	   0x01|A*|	0x32|N*| ST|
+ * 
+ *  NOTESE QUE SE ENVÍAN PRIMERO LOS BYTES MÁS SIGNIFICATIVOS (MSBY FIRST)--------------
+ * 
+ *  -¿Cómo se guardan los bytes escrios en mi I2C? (*Por un maestro)
+ *  
+ *  EJEMPLO, EL MAESTRO DESEA ESCRIBIR UN WORD EN LOS REGISTROS 0x01 CON EL VALOR 0x51AC
+ *  DE MI ESCLAVO CON DIRECCIÓN 0x1F
+ * 
+ *  el paquete I2C se eviará de la siguiente manera:
+ * 
+ *     0011111| 0| 0| 00000001| 0| 10101100| 0| 01010001| 0|
+ * | S|   DIRE| W| A|     DIRR| A|     0xAC| A|     0x51| A| ST|
+ * 
+ *  NOTESE QUE DEBE ENVÍAR PRIMERO LOS BYTES MENOS SIGNIFICATIVOS (LSBY FIRST)----------
+ * 
+ *  NOTA:
+ * 
+ *  S = START
+ *  RS = REPEATED START
+ *  ST = STOP
+ * 
+ *  DIRE = DIRECCIÓN ESCLAVO
+ *  DIRR = DIRECCIÓN REGISTRO
+ *  
+ *  W = WRITE
+ *  R = READ
+ * 
+ *  A = ACK
+ *  N = NACK
+ *  * = CONTROLADO POR EL MAESTRO (MASTER CONTROLLED)
+ * 
+ */
+
 #ifndef _I2C_SLAVE_H_
 #define	_I2C_SLAVE_H_
 #include <stdint.h>
@@ -34,8 +157,8 @@
  *  registers: Registros del periferico para ser leídos o escritos, hasta 128.
  */
 struct i2c_slave{
-	uint8_t direction	  ;
-	uint8_t registers[128];
+    uint8_t direction	  ;
+    uint8_t registers[128];
 } i2c_slave;
 
 /*-----------------------------------------------------------------------------------*/
@@ -53,10 +176,10 @@ void usi_i2c_slave(uint8_t dir);
 /* usi_i2c_save_registers_xy()
  * Descripción:
  *  Guarda un dato (data) en registros del I²C, dependiendo del tipo de dato (xy) dode:
- *	 x: - u, unsigned (Números naturales y cero)
+ *   x: - u, unsigned (Números naturales y cero)
  *      - s, signed   (Números enteros)
  * 
- *	 y: - 8,  1 byte  (byte)
+ *   y: - 8,  1 byte  (byte)
  *      - 16, 2 bytes (word)
  *      - 32, 4 bytes (dword)
  * Argumentos:
@@ -75,14 +198,14 @@ void usi_i2c_save_registers_s32(int32_t  data, uint8_t dir);
  * Descripción:
  *  Lee un byte (data) en registros del I²C escritos por el maestro, dependiendo del tipo 
  *  de dato (xy) dode:
- *	 x: - u, unsigned (Números naturales y cero)
+ *   x: - u, unsigned (Números naturales y cero)
  *      - s, signed   (Números enteros)
  * 
- *	 y: - 8,  1 byte  (byte)
+ *   y: - 8,  1 byte  (byte)
  *      - 16, 2 bytes (word)
  *      - 32, 4 bytes (dword)
  * Argumentos:
- *  -> dir:  Dirección de los registros I²C donde se desea leer el dato.
+ *  -> dir: Dirección de los registros I²C donde se desea leer el dato.
  * Retorno:
  *  <- Dato almacenado en los registros */
 uint8_t usi_i2c_read_registers_u8 (uint8_t dir);
